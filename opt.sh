20d1697fc252092cd96f448a6cdfc32ec61efd7e
softeduscn <softeduscn@gmail.com>
2024-07-22 15:16:52 +0800

    Update

--- opt.sh
+++ opt.sh
@@ -11,10 +11,12 @@
 mkdir opt
 git clone https://github.com/unifreq/openwrt_packit opt/openwrt_packit
 #kpath='https://github.com/ophub/kernel/releases/download/kernel_flippy/'
-kpath='https://github.com/breakings/OpenWrt/releases/download/kernel_stable/'
+#kpath='https://github.com/breakings/OpenWrt/releases/download/kernel_stable/'
+kpath='https://github.com/ophub/kernel/releases/download/kernel_stable/'
+
 kout='opt/openwrt_packit'
-kversion='5.15.158'
-krelease='89+o'
+kversion='5.15.163'
+krelease=''
 wget  --no-check-certificate -c -q $kpath$kversion'.tar.gz' -O $kout'/'$kversion'.tar.gz'
 tar -xvf $kout'/'$kversion'.tar.gz' -C $kout
 mv $kout'/'$kversion $kout'/kernel'
